afc279c600a1347a499b84fda3262e658c81ac25
rahatalmas <rahat.almas5599@gmail.com>
2023-08-13 17:46:55 +0600

    floyd's algo

--- c++/august/linkedlistCycle.cpp
+++ c++/august/linkedlistCycle.cpp
@@ -21,37 +21,59 @@ class LinkedList{
     public:  
       void push_back(int data);
       void push_front(int data);
-      void createLoop(); //or cycle
+      void createLoop(int pos); //or cycle
       int detectLoop();  //or cycle
       void printList();
 };
 void LinkedList::push_back(int data){
-      Node * new_node = new Node(data);
-      if(this->head == NULL){
-          this->head = new_node;
-          return;
-      }
-      Node * temp = this->head;
-      while(temp->next != NULL){
+    Node * new_node = new Node(data);
+    if(this->head == NULL){
+        this->head = new_node;
+        return;
+    }
+    Node * temp = this->head;
+    while(temp->next != NULL){
         temp = temp->next;
-      }
-      temp->next = new_node;
-      return;
+    }
+    temp->next = new_node;
+    return;
 }
 
 void LinkedList::push_front(int data){
-      Node * new_node = new Node(data);
-      if(this->head == NULL){
-         this->head = new_node;
-         return;
-      }
-      new_node->next = head;
-      this->head = new_node;
-      return;
+    Node * new_node = new Node(data);
+    if(this->head == NULL){
+        this->head = new_node;
+        return;
+    }
+    new_node->next = head;
+    this->head = new_node;
+    return;
 }
 
-int LinkedList::detectCycle(){
-      return 1;
+int LinkedList::detectLoop(){
+    Node * fastptr = this->head;
+    Node * slowptr = this->head;
+    while(slowptr!=NULL and fastptr!=NULL and fastptr->next!=NULL){
+        slowptr=slowptr->next;
+        fastptr=fastptr->next->next;
+        if(slowptr == fastptr){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void LinkedList::createLoop(int pos){
+    Node * temp = this->head;
+    Node * tail = this->head;
+    while(pos--){
+        temp = temp->next;
+    }
+    while(tail->next != NULL){
+        tail = tail->next;
+    }
+    tail->next = temp;
+    return;
 }
 
 void LinkedList::printList(){
@@ -69,5 +91,7 @@ int main(){
         l.push_back(i);
     }
     l.printList();
-
+    l.createLoop(3);
+    //l.printList(); need to update printing function for the loop...
+    cout << l.detectLoop() << endl;
 }
